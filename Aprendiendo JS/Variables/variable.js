c7d94e610152c42644569d0375482491508abe56
AlejandroRM <aloxfloyd@gmail.com>
2022-07-20 20:47:03 +0200

    Prueba de subida

---
+++ Aprendiendo JS/Variables/variable.js
@@ -0,0 +1,19 @@
+let texto1 = "Esto es una prueba"; //Esto es una variable tipo string
+
+let numero = 23; //Esto es una variable tipo numerica
+
+let indefinido; //Las variables sin valor, se veran en pantalla como "undefined"
+
+let num1 = 20;
+let num2 = 10;
+let num3 = num1 - num2; //Variable que guarda el calculo de 2 variables de tipo numerico
+
+console.log(texto1);
+
+console.log(numero);
+
+console.log(indefinido);
+
+console.log(num3);
+
+window.alert("Esto realmente es una prueba");
