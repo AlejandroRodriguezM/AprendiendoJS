a36c553a34dc05278a704090db2f646561277e59
AlejandroRM <aloxfloyd@gmail.com>
2022-07-20 20:56:58 +0200

    Variables basicas en JS

--- Aprendiendo JS/Variables/variable.js
+++ Aprendiendo JS/Variables/variable.js
@@ -2,18 +2,26 @@ let texto1 = "Esto es una prueba"; //Esto es una variable tipo string
 
 let numero = 23; //Esto es una variable tipo numerica
 
+let estado = false;
+
 let indefinido; //Las variables sin valor, se veran en pantalla como "undefined"
 
 let num1 = 20;
 let num2 = 10;
 let num3 = num1 - num2; //Variable que guarda el calculo de 2 variables de tipo numerico
 
-console.log(texto1);
+console.log(texto1); //Variable que se muestra en la consola del navegador
 
-console.log(numero);
+console.log(numero); //Variable que se muestra en la consola del navegador
 
-console.log(indefinido);
+console.log(estado); //Variable que se muestra en la consola del navegador
 
-console.log(num3);
+console.log(indefinido); //Variable que se muestra en la consola del navegador
 
-window.alert("Esto realmente es una prueba");
+console.log(num3); //Variable que se muestra en la consola del navegador
+
+document.getElementById("p1").innerHTML = "hello " + texto1; //Cambios en el .html que permite cambiar valores por las variables que queramos
+
+document.getElementById("p2").innerHTML = "Tengo " + num3 + " años"; //Cambios en el .html que permite cambiar valores por las variables que queramos
+
+window.alert("Esto realmente es una prueba"); //Muestra una alerta en pantalla con el texto o variable que queramos.
