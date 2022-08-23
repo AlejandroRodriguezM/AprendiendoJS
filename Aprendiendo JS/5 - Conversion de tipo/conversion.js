83de25cdac74beb4cfe668ceeb6dd22d378cf878
AlejandroRM <aloxfloyd@gmail.com>
2022-08-24 01:47:28 +0200

    Tutorial 5 terminado.

--- Aprendiendo JS/5 - Conversion de tipo/conversion.js
+++ Aprendiendo JS/5 - Conversion de tipo/conversion.js
@@ -20,4 +20,23 @@ console.log(typeof edad); //Convertido a tipo Number
 
 edad += 1;
 
-console.log("Tu edad es ",edad," años, feliz cumpleaños!")
+console.log("Tu edad es ",edad," años, feliz cumpleaños!");
+
+//Cambio de tipo en varias variables
+let x;
+let x2;
+let y;
+let z;
+let z2;
+
+x = Number("3.14"); //De String a Number
+x2 = Number("pizza"); //De String a Number, devolver NaN ya que 'pizza' no es un numero.
+y = String(3.14); //De Number a String
+z = Boolean(""); //Resultado 'false' ya que esta vacio
+z2 = Boolean(" "); //Resultado 'True' ya que contiene datos(Aunque sea un espacio)
+
+console.log(x, typeof x);
+console.log(x2, typeof x2);
+console.log(y, typeof y);
+console.log(z, typeof z);
+console.log(z2, typeof z2);
